8cdba355037ff74e6adf02ff4a42a90fa53d78b7
Sanzhar Karaulov <65087815+unsaiddream@users.noreply.github.com>
2026-05-18 22:53:52 +0500

    save_csv func

--- services/file_services.py
+++ services/file_services.py
@@ -6,9 +6,16 @@ class FileService:
     def save_json(filepath, data):
         with open(filepath, 'w') as file:
             json.dump(data, file, indent = 4)
-            
+
     @staticmethod
     def load_json(filepath):
         with open(filepath, 'r') as file:
             return json.load(file)
+    
+    @staticmethod
+    def save_csv(filepath, data , fieldnames):
+        with open(filepath, 'w') as file:
+            writer = csv.DictWriter(file, fieldnames = fieldnames)
+            writer.writeheader()
+            writer.writedata(data)
     
